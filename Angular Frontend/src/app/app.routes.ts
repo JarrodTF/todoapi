1b608753ea8e8854980df0c3599953f411d2f455
JarrodHome <flaxmanjt@gmail.com>
2024-11-30 15:05:56 +0200

    WIP Front end || Home and Login pages

--- Angular Frontend/src/app/app.routes.ts
+++ Angular Frontend/src/app/app.routes.ts
@@ -2,15 +2,23 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = 
 [
+    //Defaulted page to load
+    // {
+    //    path:'',
+    //    redirectTo: 'login',
+    //    pathMatch:'full' 
+    // },
+
     {
-       path:'',
-       redirectTo: 'login',
-       pathMatch:'full' 
+        path:'',
+        component:HomeComponent
     },
 
+    //Login Route with Component
     {
         path:'login',
         component:LoginComponent
--- Angular Frontend/src/app/pages/login/login.component.ts
+++ Angular Frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatInputModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  hide=true;
+  form!:FormGroup;
+  fb = inject(FormBuilder);
 
+  login(){}
+
+  ngOnInit(): void {
+    this.form = this.fb.group
+    ({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required]]
+    });
+  }
 }
